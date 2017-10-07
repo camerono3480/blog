1608b35eb9a2d92888a44d53ef43209abd936e34
Your Name <you@example.com>
2017-10-08 12:48:22 +1300

    blogs first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cameron's world</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+
+     <div class="nav">
+     <h2 class="a1">tutoriales</h2>
+      </div>
+
+      <h1 class="a51">Welcome</h1>
+
+     <div class="content">
+     	 
+     <p>hello! <br> my name is cameron im am (age) unknown. haha of course i was never gonna tell you!. anyway, welcome to my 'not so odd' blog... i really don't know what the actual fuck to put on this so called blog, i also dont know if i should swear here, maybe i should add a censor option. </p>
+
+     	 
+     </div>
+
+     <div class="footerer">
+     	 
+     </div> 
+
+  </body>
+</html>
