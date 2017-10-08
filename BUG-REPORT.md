6df8672baf5a5d73b3cb1a1c94968070114bf19c
Your Name <you@example.com>
2017-10-08 20:34:43 +1300

    some bug fixes with CoffeeScript avalibilty

--- BUG-REPORT.md
+++ BUG-REPORT.md
@@ -1,6 +1,7 @@
 bugs that need to be fixed:
 
 V: a1-2b:
-*h2a1: hover on h2.a1 area stretches across the hole webpage...fix[]
+*h2a1: hover on h2.a1 area stretches across the hole webpage...fix[h1 doesnt function properly, so i added an 'a' link tags in the h1 tags]
+* google adsense not FUCKING SHOWING!!! ahhhh!!!!
 
 V: a1-3b:
--- index.php
+++ index.php
@@ -11,26 +11,53 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+
 <link rel="stylesheet" href="css/styles.css">
+
+<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.1.2/coffee-script.min.js"></script>
+ 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
   </head>
   <body>
-
+    
+    <!-- this is the nav bar, you may call is a header -->
      <div class="nav">
-     <h2 class="a1">tutoriales</h2>
-      </div>
-
-      <h1 class="a51">Welcome</h1>
-
+       <h2><a href="#">tutoriales</a></h2> 
+       <h2><a href="#" class="sc">social</a></h2>
+       <h2><a href="#" class="hr">hire</a></h2>             
+     </div>
+       
+     <!-- coffeescript-->
+     <!-- this is unnessersy but i want to have fun with this later on :) -->
+     <script type="text/coffeescript">
+     
+     </script>
      <div class="content">
-     	 
+     <h1 class="a51">Welcome</h1>	 
      <p>hello! <br> my name is cameron im am (age) unknown. haha of course i was never gonna tell you!. anyway, welcome to my 'not so odd' blog... i really don't know what the actual fuck to put on this so called blog, i also dont know if i should swear here, maybe i should add a censor option. </p>
 
      	 
      </div>
+  
 
+      <!-- footer -->
      <div class="footerer">
      	 
      </div> 
+      
+
+          <!-- google adsense -->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ad1 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-5842551138238452"
+     data-ad-slot="9500248719"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
   </body>
 </html>
